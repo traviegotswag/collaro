2fae1406925d14b9f96d1460d8074641371744e9
traviegotswag <travisng@hotmail.sg>
2018-10-16 04:11:56 +0800

    Good to go

--- views/root.jsx
+++ views/root.jsx
@@ -6,18 +6,48 @@ class Home extends React.Component {
   render() {
     return (
 
-        <Layout>
-                <div className="login">
-                    <button name="login"><a href='/users/login'>Existing user? Login here</a></button>
-                </div>
+    <Layout>
 
-                <div className="create-account">
-                    <button name="create-account"><a href='/users/new'>New to Collaro? Create an account</a></button>
+          <div>
+            <div class='row no-gutters'>
+              <div id='test' class='col-12 col-md-6 text-container'>
+                <div class='row no-gutters h-100 justify-content-center align-items-center'>
+                  <div class='col-10 col-md-9 col-lg-8 col-xl-7'>
+                    <div class='text-header'><a href="/users/new" className="redirect2">Click here for new users</a></div>
+                        <p class='text-content'></p>
+                  </div>
                 </div>
-        </Layout>
+              </div>
+
+              <div class='col-12 col-md-6'>
+                <img class='img-fluid square' src="/images/3.jpg"/>
+              </div>
+            </div>
+
+
+            <div class='row no-gutters'>
+              <div class='order-last col-12 order-md-first col-md-6'>
+                <img class='img-fluid' src="/images/root.jpg"/>
+              </div>
+
+              <div class='order-first col-12 order-md-last col-md-6 text-container'>
+                <div class='row no-gutters h-100 justify-content-center align-items-center'>
+                  <div class='col-10 col-md-9 col-lg-8 col-xl-7'>
+                    <p class='text-header'><a href='/users/login' className="redirect2">And here, for our existing users.</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+        </div>
+
+    </Layout>
+
 
     );
   }
 }
 
 module.exports = Home;
+
+
